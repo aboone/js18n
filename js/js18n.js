45dc221b728563e1d24c76f3ab274dd05572eaa0
Alex Boone <aboone@redhat.com>
2011-06-22 15:04:59 -0400

    Simplify interface

--- js/js18n.js
+++ js/js18n.js
@@ -1,27 +1,119 @@
 // js18n - javascript internationalization framework
+//
+// Configuration Options
+// 	bundlePath 	=> URL prefix for loading bundles (default: './bundles')
+// 	loadingMode	=> 'ajax' or 'jsonp' (default: 'jsonp')
 
-if (typeof js18n == 'undefined') {
-	js18n = {};
-}
+js18n = function(config) {
 
-js18n.ResourceBundle = function(bundleName, locale, config) {
-	var bundleData;
-	var reverseBundleData;
-	var loaded = false;
+	var cache = {};
+	var listeners = [];
 
-	if (typeof config != 'object') {
-		config = {};
+	var addListener = function(requiredBundles, fn, scope) {
+		var l = {
+			bundles: requiredBundles || [],
+			fn: fn,
+			scope: scope || window
+		};
+		listeners.push(l);
+		return l;
 	}
 
-	var utils = {
-		getData : function(bundle) {
-			var fqName = bundle.getBundleName();
-			if (config.lazyLoad === true && typeof bundleData == 'undefined') {
-				var data = this.loadBundle(fqName);
-				bundleData = data ? data : {};
-			}
+	var ResourceBundle = function(bundleObj) {
+		var name = bundleObj.name;
+		var locale = bundleObj.locale;
+		var bundleData = bundleObj.data;
+		var reverseBundleData;
 
-			return bundleData;
+		return {
+			getBundleName : function() {
+				return privateUtils.getBundleName(name, locale);
+			},
+			getString : function(key, defaultToKey) {
+				var val = bundleData[key];
+				if (val) {
+					return val;
+				} else if (defaultToKey !== false) {
+					return key;
+				}
+			},
+			findKey : function(str) {
+				if (reverseBundleData == undefined) {
+					reverseBundleData = {};
+					for (var k in bundleData) {
+						if (bundleData.hasOwnProperty(k)) {
+							reverseBundleData[bundleData[k]] = k;
+						}
+					}
+				}
+
+				return reverseBundleData[str];
+			}
+		};
+	};
+
+	var MessageSource = function(bundleNames, locale) {
+		if (typeof bundleNames == 'string') {
+			return new MessageSource([bundleNames], locale);
+		}
+
+		var bundles = [];
+		for (var i=0; i<bundleNames.length; i++) {
+			bundles.push(privateUtils.getBundleName(bundleNames[i], locale));
+		}
+
+		return {
+			getBundles : function() {
+				return bundles;
+			},
+			getString : function(key, defaultToKey) {
+				for (var i=0; i<bundles.length; i++) {
+					var b = js18n.getBundle(bundles[i]);
+					var val = b.getString(key, false);
+					if (val) {
+						return val;
+					}
+				}
+				if (defaultToKey !== false) {
+					return key;
+				}
+			},
+			findKey : function(str) {
+				for (var i=0; i<bundles.length; i++) {
+					var b = js18n.getBundle(bundles[i]);
+					var val = b.findKey(str);
+					if (val) {
+						return val;
+					}
+				}
+			}
+		};
+	}
+	MS = MessageSource;
+
+	var privateUtils = {
+		findTextNodes : function(root) {
+			var textNodes = [];
+			if (!root) {
+				return textNodes;
+			} else if (root.nodeType == 3) {
+				if (root.nodeValue && root.nodeValue.replace(/(^\s+|\s+$)/g, '')) {
+					textNodes.push(root);
+				}
+			} else if (root && root.childNodes) {
+				var nodes = root.childNodes;
+				for (var i=0; i<nodes.length; i++) {
+					textNodes = textNodes.concat( this.findTextNodes(nodes[i]) );
+				}
+			}
+			return textNodes;
+		},
+		getBundleName : function(bundle, locale) {
+			return bundle + '_' + locale;
+		},
+		getBundleUrl : function(bundleName) {
+			var base = config.bundlePath || './bundles';
+			return base + '/' + bundleName + '.js';
 		},
 		getXHR : function() {
 			var xhr;
@@ -34,10 +126,6 @@ js18n.ResourceBundle = function(bundleName, locale, config) {
 			} catch (e) {}
 
 			return xhr;
-		},
-		getBundleUrl : function(bundleName) {
-			var base = config.bundlePath || './bundles';
-			return base + '/' + bundleName + '.js';
 		},
 		loadBundle : function(bundleName) {
 			var xhr = this.getXHR();
@@ -53,146 +141,126 @@ js18n.ResourceBundle = function(bundleName, locale, config) {
 				try {
 					eval(xhr.responseText);
 					if (typeof bundle == 'object' && bundle.data) {
-						return bundle.data;
+						js18n.bundleLoaded(bundle);
 					}
 				} catch (e) {}
 			}
 		},
-		loadBundleAsync : function(bundleName, bundleObj) {
-			js18n.LoaderQueue.add(bundleName, bundleObj);
+		loadBundleAsync : function(bundleName) {
 			var scr = document.createElement('script');
 			scr.setAttribute('src', this.getBundleUrl(bundleName));
 			document.body.appendChild(scr);
+		},
+		parseBundleName : function(bundleName) {
+			var parts = bundleName.split('_');
+			return {name: parts[0], locale: parts[1]};
+		},
+		testListener : function(l) {
+			var satisfied = true;
+			for (var i=0; i<l.bundles.length; i++) {
+				satisfied = satisfied && js18n.getBundle(l.bundles[i]);
+			}
+
+			if (satisfied) {
+				l.fn.apply(l.scope);
+			}
+
+			return satisfied;
 		}
 	}
 
-	var b = {
-		setLocale : function(loc) {
-			locale = loc;
-		},
-		getBundleName : function() {
-			return bundleName + '_' + locale;
-		},
-		isLoaded : function() {
-			return loaded;
-		},
-		setBundleData : function(data) {
-			bundleData = data;
-			loaded = true;
-		},
-		getString : function(key, defaultToKey) {
-			var val = utils.getData(this)[key];
-			if (val) {
-				return val;
-			} else if (defaultToKey !== false) {
-				return key;
+	return {
+		bundleLoaded : function(bundle) {
+			var rb = new ResourceBundle(bundle);
+			cache[rb.getBundleName()] = rb;
+
+			for (var j=0; j<listeners.length; j++) {
+				if (privateUtils.testListener(listeners[j])) {
+					listeners.splice(j--, 1);
+				}
 			}
 		},
-		findKey : function(str) {
-			if (reverseBundleData == undefined) {
-				var data = utils.getData(this);
-				reverseBundleData = {};
-				for (var k in data) {
-					if (data.hasOwnProperty(k)) {
-						reverseBundleData[data[k]] = k;
+		convert : function(root, bundleNames, srcLocale, destLocale, callback, scope) {
+			if (typeof bundleNames == 'string') {
+				return this.convert(root, [bundleNames], srcLocale, destLocale, callback, scope);
+			}
+
+			var src = new MessageSource(bundleNames, srcLocale);
+			var dest = new MessageSource(bundleNames, destLocale);
+
+			var cb = function() {
+				if (typeof callback == 'function') {
+					callback.apply(scope || window, [src, dest]);
+				}
+			}
+
+			if (srcLocale == destLocale) {
+				cb();
+			} else {
+
+				var bundles = Array.concat(src.getBundles(), dest.getBundles());
+				
+				var that = this;
+				this.load(bundles, function() {
+					var nodes = privateUtils.findTextNodes(root);
+					for (var i=0; i<nodes.length; i++) {
+						var val = nodes[i].nodeValue;
+
+						var trVal = that.convertTerm(val, src, dest);
+						if (trVal) {
+							nodes[i].nodeValue = trVal;
+						}
+					}
+
+					cb();
+				});
+			}
+		},
+		convertTerm : function(term, srcMessageSource, destMessageSource) {
+			var key = srcMessageSource.findKey(term.replace(/(^\s+|\s+$)/g, ''));
+			if (key) {
+				return destMessageSource.getString(key, false);
+			}
+		},
+		getBundle : function(name, locale) {
+			if (locale) {
+				name = privateUtils.getBundleName(name, locale);
+			}
+			return cache[name];
+		},
+		load : function(bundles, fn, scope) {
+			if (typeof bundles == 'string') {
+				this.load([bundles], fn, scope);
+			}
+
+			if (fn) {
+				var listener = addListener(bundles, fn, scope);
+			}
+			
+			for (var i=0; i<bundles.length; i++) {
+				var name = bundles[i];
+				if (!cache.hasOwnProperty(name)) {
+					cache[name] = null;
+					if (config.loadingStyle == 'ajax') {
+						privateUtils.load(name);
+					} else {
+						privateUtils.loadBundleAsync(name);
 					}
 				}
 			}
 
-			return reverseBundleData[str];
-		}
-	};
-
-	if (!config.lazyLoad) {
-		utils.loadBundleAsync(b.getBundleName(), b);
-	}
-
-	return b;
-};
-
-js18n.Utils = function() {
-	var findTextNodes = function(root) {
-		var textNodes = [];
-		if (!root) {
-			return textNodes;
-		} else if (root.nodeType == 3) {
-			if (root.nodeValue && root.nodeValue.replace(/(^\s+|\s+$)/g, '')) {
-				textNodes.push(root);
-			}
-		} else if (root && root.childNodes) {
-			var nodes = root.childNodes;
-			for (var i=0; i<nodes.length; i++) {
-				textNodes = textNodes.concat( findTextNodes(nodes[i]) );
-			}
-		}
-		return textNodes;
-	};
-
-	return {
-		localize : function(root, bundle) {
-			var nodes = findTextNodes(root);
+			privateUtils.testListener(listener);
+		},
+		localize : function(root, bundle, callback, scope) {
+			var nodes = privateUtils.findTextNodes(root);
 			for (var i=0; i<nodes.length; i++) {
 				var val = nodes[i].nodeValue;
 				nodes[i].nodeValue = bundle.getString(val);
 			}
-		},
-		convertTerm : function(term, sourceBundle, destBundle) {
-			if (sourceBundle.getBundleName() == destBundle.getBundleName()) {
-				return term;
-			}
 
-			var key = sourceBundle.findKey(term.replace(/(^\s+|\s+$)/g, ''));
-			if (key) {
-				return destBundle.getString(key, false);
-			}
-		},
-		convert : function(root, sourceBundle, destBundle) {
-			if (sourceBundle.getBundleName() == destBundle.getBundleName()) {
-				return;
-			}
-
-			var nodes = findTextNodes(root);
-			for (var i=0; i<nodes.length; i++) {
-				var val = nodes[i].nodeValue;
-				var trVal = this.convertTerm(val, sourceBundle, destBundle);
-				if (trVal) {
-					nodes[i].nodeValue = trVal;
-				}
+			if (typeof callback == 'function') {
+				callback.apply(scope || window);
 			}
 		}
 	};
-}();
-
-js18n.bundleLoaded = function(bundle) {
-	js18n.LoaderQueue.process(bundle);
-};
-
-js18n.LoaderQueue = function() {
-	return {
-		queue : {},
-		listeners : [],
-		add : function(name, bundle) {
-			if (!this.queue[name]) {
-				this.queue[name] = [];
-			}
-			this.queue[name].push(bundle);
-		},
-		addListener : function(fn) {
-			this.listeners.push(fn);
-		},
-		process : function(bundle) {
-			var fqn = bundle.name + '_' + bundle.locale;
-			var q = this.queue[fqn];
-			if (q) {
-				for (var i=0; i<q.length; i++) {
-					q[i].setBundleData(bundle.data);
-				}
-			}
-			delete this.queue[fqn];
-			
-			for (var j=0; j<this.listeners.length; j++) {
-				this.listeners[j](fqn, bundle);
-			}
-		}
-	};
-}();
+}(typeof js18nConfig == 'object' ? js18nConfig : {});
